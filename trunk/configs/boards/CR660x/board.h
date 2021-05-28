6c79f643cb0ca42b97beb5c9562211c7e7f37fdf
hanwckf <hanwckf@vip.qq.com>
2021-05-29 00:24:42 +0800

    boards: add CR660x

---
+++ trunk/configs/boards/CR660x/board.h
@@ -0,0 +1,20 @@
+#define BOARD_PID		"CR660x"
+#define BOARD_NAME		"CR660x"
+#define BOARD_DESC		"CR660x Wireless Router"
+#define BOARD_VENDOR_NAME	"Beijing Xiaomi Technology Co., Ltd."
+#define BOARD_VENDOR_URL	"http://www.mi.com/"
+#define BOARD_MODEL_URL		"http://www.mi.com/"
+#define BOARD_BOOT_TIME		20
+#define BOARD_FLASH_TIME	120
+
+#define BOARD_HAS_5G_11AC		1
+#define BOARD_HAS_5G_11AX		1
+#define BOARD_HAS_2G_11AX		1
+#define BOARD_NUM_ANT_5G_TX		2
+#define BOARD_NUM_ANT_5G_RX		2
+#define BOARD_NUM_ANT_2G_TX		2
+#define BOARD_NUM_ANT_2G_RX		2
+#define BOARD_NUM_ETH_LEDS		0
+#define BOARD_NUM_ETH_EPHY		4
+#define BOARD_HAS_EPHY_L1000	1
+#define BOARD_HAS_EPHY_W1000	1
