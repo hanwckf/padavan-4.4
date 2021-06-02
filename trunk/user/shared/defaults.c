747d34b6d32be79ff41ab9cca9fd217892ff6ad0
hanwckf <hanwckf@vip.qq.com>
2021-06-02 14:02:47 +0800

    shared: enable shortcut-fe by default

--- trunk/user/shared/defaults.c
+++ trunk/user/shared/defaults.c
@@ -760,7 +760,7 @@ struct nvram_pair router_defaults[] = {
 	{ "hw_nat_mode", "1" },
 	{ "sw_nat_mode", "0" },
 #if defined(USE_SFE)
-	{ "sfe_enable", "0" },
+	{ "sfe_enable", "2" },
 #endif
 	{ "fw_syn_cook", "0" },
 	{ "fw_mac_drop", "0" },
