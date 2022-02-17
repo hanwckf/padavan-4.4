eec9d2b1649c5007c077f5cc206e1cf536669874
vb1980 <vb1980@sina.com>
2022-02-17 10:14:36 +0800

    add MSG1500 support, everything from https://github.com/240038901/padavan-4.4.git

---
+++ trunk/configs/boards/MSG1500/board.h
@@ -0,0 +1,21 @@
+/* MSG1500 */
+
+#define BOARD_PID		"MSG1500"
+#define BOARD_NAME		"MSG1500"
+#define BOARD_DESC		"RAISECOM MSG1500 Wireless Router"
+#define BOARD_VENDOR_NAME	"RAISECOM"
+#define BOARD_VENDOR_URL	"http://www.raisecom.com.cn/"
+#define BOARD_MODEL_URL		"http://www.raisecom.com.cn/"
+#define BOARD_BOOT_TIME		30
+#define BOARD_FLASH_TIME	120
+
+#define BOARD_HAS_5G_11AC	1
+#define BOARD_NUM_ANT_5G_TX	2
+#define BOARD_NUM_ANT_5G_RX	2
+#define BOARD_NUM_ANT_2G_TX	2
+#define BOARD_NUM_ANT_2G_RX	2
+#define BOARD_NUM_ETH_LEDS	1
+#define BOARD_HAS_EPHY_L1000	1
+#define BOARD_HAS_EPHY_W1000	1
+#undef BOARD_NUM_UPHY_USB3      0
+#define BOARD_USB_PORT_SWAP	1
