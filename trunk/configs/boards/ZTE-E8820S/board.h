e701a7a8c128d64507a680afad32327de6748adc
vb1980 <vb1980@sina.com>
2022-02-17 20:40:13 +0800

    add ZTE-E8820S support

---
+++ trunk/configs/boards/ZTE-E8820S/board.h
@@ -0,0 +1,23 @@
+/* ZTE-E8820S */
+
+#define BOARD_PID		"ZTE-E8820S"
+#define BOARD_NAME		"ZTE-E8820S"
+#define BOARD_DESC		"ZTE E8820S Wireless Router"
+#define BOARD_VENDOR_NAME	"ZTE Technology"
+#define BOARD_VENDOR_URL	"http://www.ztehome.com.cn/"
+#define BOARD_MODEL_URL	"http://www.ztehome.com.cn/"
+#define BOARD_BOOT_TIME	30
+#define BOARD_FLASH_TIME   	120
+
+#define BOARD_HAS_5G_11AC	1
+#define BOARD_NUM_ANT_5G_TX	2
+#define BOARD_NUM_ANT_5G_RX	2
+#define BOARD_NUM_ANT_2G_TX	2
+#define BOARD_NUM_ANT_2G_RX	2
+#define BOARD_NUM_ETH_LEDS	1
+#define BOARD_HAS_EPHY_L1000	1
+#define BOARD_HAS_EPHY_W1000	1
+#define BOARD_NUM_UPHY_USB3	1
+#define BOARD_GPIO_PWR_USB	11
+#define BOARD_GPIO_PWR_USB_ON	1
+#define BOARD_USB_PORT_SWAP	0
