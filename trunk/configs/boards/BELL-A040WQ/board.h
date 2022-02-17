09d100898da4b7d71126ad8b2b6c344388da0986
vb1980 <vb1980@sina.com>
2022-02-17 10:17:53 +0800

    add BELL-A040WQ support, copy from https://github.com/240038901/padavan-4.4.git, thanks

---
+++ trunk/configs/boards/BELL-A040WQ/board.h
@@ -0,0 +1,20 @@
+/* BELL-A040WQ */
+#define BOARD_PID		"BELL-A040WQ"
+#define BOARD_NAME		"BELL-A040WQ"
+#define BOARD_DESC		"nokia-sbell A-040W-Q Wireless Router"
+#define BOARD_VENDOR_NAME	"nokia-sbell"
+#define BOARD_VENDOR_URL	"http://www.nokia-sbell.com/"
+#define BOARD_MODEL_URL		"http://www.nokia-sbell.com/"
+#define BOARD_BOOT_TIME		30
+#define BOARD_FLASH_TIME	120
+
+#define BOARD_HAS_5G_11AC	1
+#define BOARD_NUM_ANT_5G_TX	2
+#define BOARD_NUM_ANT_5G_RX	2
+#define BOARD_NUM_ANT_2G_TX	2
+#define BOARD_NUM_ANT_2G_RX	2
+#define BOARD_NUM_ETH_LEDS	1
+#define BOARD_HAS_EPHY_L1000	1
+#define BOARD_HAS_EPHY_W1000	1
+#undef BOARD_NUM_UPHY_USB3      0
+#define BOARD_USB_PORT_SWAP	1
