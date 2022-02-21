ec225a1f523bd6cafa6ceeafe19634dfd36146b2
chongshengB <306835258@qq.com>
2022-02-21 06:29:13 +0000

    add MI-R3P support pb-boot. use MI-R3P-PB to build

---
+++ trunk/configs/boards/MI-R3P-PB/board.h
@@ -0,0 +1,21 @@
+/* Xiaomi Mi R3P */
+
+#define BOARD_PID		"MI-R3P"
+#define BOARD_NAME		"MI-R3P"
+#define BOARD_DESC		"Xiaomi Mi R3P Wireless Router"
+#define BOARD_VENDOR_NAME	"Beijing Xiaomi Technology Co., Ltd."
+#define BOARD_VENDOR_URL	"http://www.mi.com/"
+#define BOARD_MODEL_URL		"http://www.mi.com/miwifir3p"
+#define BOARD_BOOT_TIME		20
+#define BOARD_FLASH_TIME	120
+
+#define BOARD_HAS_5G_11AC		1
+#define BOARD_NUM_ANT_5G_TX		4
+#define BOARD_NUM_ANT_5G_RX		4
+#define BOARD_NUM_ANT_2G_TX		4
+#define BOARD_NUM_ANT_2G_RX		4
+#define BOARD_NUM_ETH_LEDS		1
+#define BOARD_NUM_ETH_EPHY		4
+#define BOARD_HAS_EPHY_L1000		1
+#define BOARD_HAS_EPHY_W1000		1
+#define BOARD_NUM_UPHY_USB3		1
